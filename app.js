564050763a9b8bca5aa9628064dc78be29e08e0f
whoissubedibibek <subedibibek00@outlook.com>
2022-01-11 23:10:34 +0545

    removed console log

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 var randomNumber1 = Math.floor((Math.random()*6+1));
-console.log(randomNumber1);
-
 var randomImage1 = "/images/"+"dice"+randomNumber1+".png";
-console.log(randomImage1)
-
 var randomNumber2 = Math.floor((Math.random()*6+1));
-console.log(randomNumber2);
-
 var randomImage2 = "/images/"+"dice"+randomNumber2+".png";
-console.log(randomImage2)
 
 document.querySelectorAll("img")[0].setAttribute("src",randomImage1);
 document.querySelectorAll("img")[1].setAttribute("src",randomImage2);
